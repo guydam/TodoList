fa0af3fcc68bf8c01d8e186d4500b738c4e8bfa7
guydam <guydam1@gmail.com>
2014-02-24 21:33:04 +0200

    Documentation and icons

--- gen/il/ac/huji/todolist/R.java
+++ gen/il/ac/huji/todolist/R.java
@@ -24,6 +24,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_new=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int todo_icon=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout=0x7f080003;
--- src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -18,10 +18,11 @@ import android.widget.Toast;
 
 public class TodoListManagerActivity extends Activity {
 
-    public static final String PREFS_NAME = "MyPrefsFile";
-    public static final String STRING_SET_NAME = "StringSetNum_";
-    public static final String ITEMS_COUNT_NAME = "ItemsCount";
-    
+	// Name Definitions for the shared preferences mechanism
+	public static final String PREFS_NAME = "MyPrefsFile";
+	public static final String STRING_SET_NAME = "StringSetNum_";
+	public static final String ITEMS_COUNT_NAME = "ItemsCount";
+
 	ArrayList<String> todoList;
 	AltColorAdapter todoListAdapter;
 
@@ -33,18 +34,22 @@ public class TodoListManagerActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo_list_manager);
 
+		// Define the todo list array and get local data if any
 		todoList = new ArrayList<String>();
 		getListFromSharedPreferences();
 
+		// Get references to views
 		todoListView = (ListView) findViewById(R.id.lstTodoItems);
 		newItemEditText = (EditText) findViewById(R.id.edtNewItem);
 
+		// Register the context menu to the list view
 		registerForContextMenu(todoListView);
-		
-		todoListAdapter = new AltColorAdapter(this,android.R.layout.simple_list_item_1, todoList);
+
+		// Create the list adapter and set it with the list view
+		todoListAdapter = new AltColorAdapter(this,
+				android.R.layout.simple_list_item_1, todoList);
 		todoListView.setAdapter(todoListAdapter);
 		todoListAdapter.notifyDataSetChanged();
-
 	}
 
 	@Override
@@ -53,26 +58,37 @@ public class TodoListManagerActivity extends Activity {
 		getMenuInflater().inflate(R.menu.todo_list_manager, menu);
 		return true;
 	}
-	
+
+	/**
+	 * Called then options item is selected
+	 */
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case (R.id.menuItemAdd) : 
+		case (R.id.menuItemAdd):
 			String newItemStr = newItemEditText.getText().toString();
-			if (newItemStr.length()!=0) {
+			if (newItemStr.length() != 0) {
 				addNewItem(newItemStr);
 				newItemEditText.setText("");
-				((InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+				((InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE))
+						.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY,
+								0);
 				showToast(getString(R.string.toast_item_added));
 			} else {
 				showToast(getString(R.string.toast_no_item));
 				return false;
 			}
 			return true;
-		};
-		
+		}
+		;
+
 		return super.onOptionsItemSelected(item);
 	};
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+
+	/**
+	 * Called when the context menu is created (long pressing the list view)
+	 */
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		getMenuInflater().inflate(R.menu.todo_list_context_menu, menu);
 
@@ -81,77 +97,107 @@ public class TodoListManagerActivity extends Activity {
 		menu.setHeaderTitle(titleStr);
 	}
 
+	/**
+	 * Called when a context menu item is selected
+	 */
 	public boolean onContextItemSelected(MenuItem item) {
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-		
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+
 		switch (item.getItemId()) {
-		case R.id.menuItemDelete: 			
+		case R.id.menuItemDelete:
 			removeItem((int) info.position);
 			return true;
-		};
+		}
+		;
 
-		return super.onContextItemSelected(item); 
+		return super.onContextItemSelected(item);
 	};
 
-	private String getItemContent (int position) {
-		if (position < todoList.size() && position >=0) {
+	/**
+	 * Get the todo item content from the array
+	 * 
+	 * @param position
+	 *            The given position index to get
+	 * @return The todo item in the given position
+	 */
+	private String getItemContent(int position) {
+		if (position < todoList.size() && position >= 0) {
 			return todoList.get(position);
 		}
 		return "";
 	}
-	
-	private boolean removeItem (int position) {
-		if (position < todoList.size() && position >=0) {
+
+	/**
+	 * Remove item from the todo array
+	 * 
+	 * @param position
+	 *            the given index to remove
+	 * @return true if the item was removed, false otherwise
+	 */
+	private boolean removeItem(int position) {
+		if (position < todoList.size() && position >= 0) {
 			todoList.remove(position);
 			todoListAdapter.notifyDataSetChanged();
 			return true;
 		}
-		return false; 
+		return false;
 	}
-	
+
+	/**
+	 * Add a new todo item to the array
+	 * 
+	 * @param newItemStr
+	 *            The item to add
+	 */
 	private void addNewItem(String newItemStr) {
 		todoList.add(newItemStr);
 		todoListAdapter.notifyDataSetChanged();
 	}
-	
-	private void showToast (String text) {
+
+	/**
+	 * Display a short toast in the screen
+	 * 
+	 * @param text
+	 *            the given text to display in the toast
+	 */
+	private void showToast(String text) {
 		Context context = getApplicationContext();
 		int duration = Toast.LENGTH_SHORT;
 
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
 	}
-	
-	private void getListFromSharedPreferences () {
-		
-	    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		
-		int listSize = settings.getInt(ITEMS_COUNT_NAME, 0);
-		
-		for (int i = 0 ; i<listSize ; i++) {
-			todoList.add(settings.getString(STRING_SET_NAME+i, ""));
-		}
-		
-		
-		
-	}
-	@Override
-    protected void onStop(){
-       super.onStop();
 
-      // We need an Editor object to make preference changes.
-      // All objects are from android.context.Context
-      SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-      SharedPreferences.Editor editor = settings.edit();
-      
-      editor.putInt(ITEMS_COUNT_NAME, todoList.size());
-      
-      for (int i = 0; i<todoList.size(); i++) {
-    	  editor.putString(STRING_SET_NAME+i, todoList.get(i));
-      }
-      
-      // Commit the edits!
-      editor.commit();
-    }
-	
+	/**
+	 * Retrieve the todo items array from the local shared preferences
+	 */
+	private void getListFromSharedPreferences() {
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		int listSize = settings.getInt(ITEMS_COUNT_NAME, 0);
+
+		for (int i = 0; i < listSize; i++) {
+			todoList.add(settings.getString(STRING_SET_NAME + i, ""));
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		// Get the shared preferences editor
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+
+		// Put the items count into shared preferences
+		editor.putInt(ITEMS_COUNT_NAME, todoList.size());
+		
+		// Put all the todo items into the shared preferences
+		for (int i = 0; i < todoList.size(); i++) {
+			editor.putString(STRING_SET_NAME + i, todoList.get(i));
+		}
+
+		// Commit the edits!
+		editor.commit();
+	}
 }
